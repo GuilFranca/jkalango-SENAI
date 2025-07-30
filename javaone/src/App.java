ad9b08a041db0b2412348042f1c6458a8ec1b990
Guilherme França <131270776+GuilFranca@users.noreply.github.com>
2025-07-30 11:48:47 -0300

    Instanciando objetos

--- javaone/src/App.java
+++ javaone/src/App.java
@@ -2,9 +2,19 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
-        Genero generoTerror = new Genero("Terror");
+        Genero g1 = new Genero("Aventura", "Jogo focado em exploração");
+        Genero g2 = new Genero("Romance","Safadeza, nheco nheco , fuqui fuqui");
+        Genero g3 = new Genero("Terror", "Medo, fantasma, safadeza");
+        Genero g4 = new Genero("Corrida","Vrummm, vrummmm, iihihiiii, poooow");
+        Genero g5 = new Genero("Shooter","piipipiiiipiipiip boom");
+        Genero g6 = new Genero("Sci-fi", "Monstro, espaço , futuro");
 
-        System.out.println(generoTerror.getNome());
+        g1.MostrarGenero();
+        g2.MostrarGenero();
+        g3.MostrarGenero();
+        g4.MostrarGenero();
+        g5.MostrarGenero();
+        g6.MostrarGenero();
 
     }
 }
--- javaone/src/Genero.java
+++ javaone/src/Genero.java
@@ -1,9 +1,29 @@
 public class Genero {
 
+    // Exercicio
+    // Crie o atributo descrição
+    // Crie o get e set da descrição
+    // Crie um construtor com todos os atributos
+    // Nome e descrição
+
     private String nome;
+    private String descricao;
+
+    public Genero() {
+
+    }
 
     public Genero(String nome) {
         this.nome = nome;
+    }
+
+    public Genero(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public void MostrarGenero() {
+        System.out.println("Nome: " + this.nome + "\nDescrição: " + this.descricao);
     }
 
     public String getNome() {
@@ -12,6 +32,14 @@ public class Genero {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
     }
 
 }
